6c1b02395cc7e85f82523a5d74bd706019c79f59
Pablo Corcho <corchocaballeropablo@gmail.com>
2023-10-07 11:15:49 +0200

    refactor QC generic functions

--- src/ifs_tools/QC/QCtestBase.py
+++ src/ifs_tools/QC/QCtestBase.py
@@ -1,7 +1,11 @@
-from weave.html_tools.utils import HTMLPage
+import yaml
+
+from ifs_tools.html_tools.utils import HTMLPage
 
 class QCtestBase(object):
     """Base class for Quality Control plots."""
+    data_container = None
+
     def __init__(self, data_level, **kwargs):
         self.data_level = data_level
         self.name = kwargs.get("name", "*name*?")
@@ -14,3 +18,24 @@ class QCtestBase(object):
             )
         else:
             self.html_page = None
+
+    def load_yml_file(self, file_path):
+        with open(file_path, 'r') as file:
+            content = yaml.safe_load(file)
+        return content
+
+    def check_header(self, key_dict, hdul_idx=0):
+        info = self.data_container.get_from_header(key_dict.keys(),
+                                         hdul_idx=hdul_idx)
+        checks = {}
+        for k, v, in info.items():
+            okey = True
+            if type(v) is not str and key_dict[k] != "None":
+                okey = key_dict[k][0] < v < key_dict[k][1]
+            elif type(v) is str and key_dict[k] != "None":
+                okey = v == key_dict[k][0]
+            else:
+                okey = "N/A"
+            checks[k] = okey
+            # print(f"{k}: {v} -- Okey: {okey}")
+        return info, checks
